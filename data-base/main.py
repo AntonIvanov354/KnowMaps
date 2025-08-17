2ba3840fbc5e456bec56354bc7232527f223bd2e
Anton Ivanov <adivanov2809@gmail.com>
2025-08-17 22:42:40 +0300

    Крч, проблема в таймингах и я хз как её фиксить, тепер дол Москвы

--- data-base/main.py
+++ data-base/main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI#, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List
+#from typing import List
+import sqlite3
+
+connection = sqlite3.connect("data-base-KM.db")
+data_base = connection.cursor()
 
 app = FastAPI()
 
 class DBModel(BaseModel):
     ip: str
     name: str
-    email: str
+    email: str  
 
-db_list: List[DBModel] = []
+class checking_registration(BaseModel):
+    ip: str
+
+#db_list: List[DBModel] = []
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,9 +32,39 @@ app.add_middleware(
   #  return {"message": f"Ваш айпи адрес {client_ip}"}
 @app.post("/base/creat")
 async def creat_item(item: DBModel):
-    print("Сосо пж")
+    ip_base = item.ip
+    name_base = item.name
+    email_base = item.email
+
+    data_base.execute("INSERT INTO Users (ip, name, email) VALUES (?, ?, ?)", (ip_base, name_base, email_base))
+
+    print("ХЗ, вроде данные сохранил")
     return {"message": "Хз вроде сохранил данные", "item": item}
 
 @app.get("/base")
-async def rez(item: DBModel):
-    return{"message": "Хз вроде сохранил данные", "item": item}
+async def rez():
+    data_base.execute("SELECT * FROM Users WHERE ip = ?",  ("ййй",))
+    rezult = data_base.fetchall()
+    print(rezult)
+
+    connection.commit()
+    connection.close()
+
+    return{"message": f"Хз вроде сохранил данные {rezult}"}
+
+@app.post("/akkaynt/ip_user")
+async def proverka_ip(item: checking_registration):
+    ip_us = item.ip
+    print(ip_us)
+    #try:
+    data_base.execute("SELECT * FROM Users WHERE ip = ?", (ip_us,))
+    result = data_base.fetchall()
+
+    if not result:
+        print(f"Ошибка данные не загрузили:{result}")
+        return{"message:" "Ошибка извлечения данных по ip адрему пользователя, повторите позже!"}
+    else:
+        print(f"Нет ошибка данные загрузили:{result}")
+        return{"message:": result}
+  #  except:
+      #  return{"message:" "Ошибка извлечения данных по ip адрему пользователя, повторите позже!"}
--- java/akkaynt.js
+++ java/akkaynt.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+/*document.addEventListener("DOMContentLoaded", function(){
     const knopka = document.getElementById("knopka")
     const knopka2 = document.getElementById("knopka2")
        /* fetch("http://127.0.0.1:8000/data-base/ip")
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(){
         Users_ip.user_ip = data.ip
     })
 */
-        knopka.addEventListener("click", function() {
+      /*  knopka.addEventListener("click", function() {
             const url = "http://127.0.0.1:8000/base/creat"
             let date = {
                 ip:"ййй",
@@ -39,11 +39,66 @@ document.addEventListener("DOMContentLoaded", function(){
         
     })*/
 
-knopka2.addEventListener("click", function() {
+/*knopka2.addEventListener("click", function() {
     fetch("http://127.0.0.1:8000/base")
         .then(response => response.json())
         .then(data => {
             console.log(data.message)
         })
- })
-})
+ })*/
+//})
+document.addEventListener("DOMContentLoaded", function(){
+    proverka_akkaynta()
+        async function proverka_akkaynta() {
+            let ip_user = null
+            /*const response = await fetch("https://api.ipify.org?format=json") 
+            if (!response.ok){
+                throw new Error(`ОШИБКА В ПОЛУЧЕНИИ ip-адреса ПОЛЬЗОВАТЕЛЯ: ${response.status}`)
+            }*/
+           await fetch("https://api.ipify.org?format=json")
+            .then(response => response.json())
+            .then(data => {
+                ip_user = data.ip
+            })
+
+            console.log(ip_user)
+            async function otpravka_data(){
+                const url = "http://127.0.0.1:8000/akkaynt/ip_user"
+                let transfer_data = {
+                    ip: ip_user
+                }
+                console.log(transfer_data)
+                fetch("http://127.0.0.1:8000/akkaynt/ip_user", {
+                    method: "POST",
+                    headers:{
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(transfer_data)
+                })
+            }
+        }
+        /*fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            const ip_isers = data.ip;
+            console.log(ip_isers)
+            })
+        
+        const url = "http://127.0.0.1:8000/akkaynt/ip_user"
+        let transfer_data = {
+            ip: ip_isers
+        }
+        if (ip_isers){
+            fetch(url, {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(transfer_data)
+            })
+        }
+        else{
+            console.log("ОШИБКА")
+        }*/
+
+});
